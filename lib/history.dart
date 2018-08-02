4f86a62ee6ffe01a78c332ca4bac426c9dca3bcf
Steven Ortiz <sortiz832@gmail.com>
2018-08-01 20:40:30 -0500

    Documented the history structure

--- lib/history.dart
+++ lib/history.dart
@@ -1,63 +1,82 @@
 import 'dart:collection';
 import 'dart:io';
 import 'package:path/path.dart' as Path;
-import 'package:path_provider/path_provider.dart';
+import 'package:path_provider/path_provider.dart' as Provider;
 
+/// An iterable data structure that maintains the user's search history. The
+/// search history will persist across sessions through a history file that
+/// will be read on construction and overwritten whenever a change occurs
+/// (asynchronously). There is a very small window where an instance may be
+/// modified before the history file has been read which will affect the order
+/// of the search queries. In the unlikely event that the history file cannot
+/// be accessed, the search history will continue to work but will not persist
+/// across sessions. In the interest of consistency and performance, only one
+/// instance of this data structure should exist at any given time.
 class History extends IterableMixin<String> {
-  Set<String> _history;
+  /// The name of the history file (the full path is determined at runtime).
+  static final _name = 'history';
+
+  /// The history set is implemented as an insertion order hash set.
+  Set<String> _history = Set();
+
+  /// The full path of the history file is asynchronously set.
   File _file;
 
+  /// Schedules an asynchronous task to load the history file.
   History() {
-    _history = Set();
-    _open();
+    () async {
+      // Compute the full path of the history file
+      var directory = await Provider.getApplicationDocumentsDirectory();
+      var location = Path.join(directory.path, _name);
+
+      try {
+        // Create the file (nondestructive)
+        _file = await File(location).create();
+      } on FileSystemException {
+        print('The history file could not be accessed');
+      }
+
+      if(_file != null) {
+        // Load the history file into the history set
+        var lines = await _file.readAsLines();
+        _history.addAll(lines);
+      }
+    }();
   }
 
-  void _open() async {
-    var directory = await getApplicationDocumentsDirectory();
-    var location = Path.join(directory.path, 'history');
-    try {
-      _file = await File(location).create();
-    } on FileSystemException {
-      print('The history file could not be opened');
-    }
-    if(_file != null) {
-      _read();
-    }
-  }
-
-  void _read() async {
-    var lines = await _file.readAsLines();
-    lines.retainWhere((line) => line.length > 0);
-    _history.addAll(lines);
-  }
-
-  void _write() async {
-    await _file.writeAsString(_history.join('\n'));
-  }
-
+  /// Returns an iterator over the history set.
   @override
   Iterator<String> get iterator {
     return _history.iterator;
   }
 
+  /// Returns the length of the history set.
   @override
   int get length {
     return _history.length;
   }
 
+  /// Checks if the history set contains the `value`.
   @override
   bool contains(Object value) {
     return _history.contains(value);
   }
 
+  /// Adds the `value` to the history set and updates the history file. If the
+  /// history set already contains the `value`, it is removed and reinserted.
   bool add(String value) {
+    // Update the history set to reflect the order of search queries
     if(_history.contains(value)) {
       _history.remove(value);
     }
     _history.add(value);
+
+    // The history file must be completely overwritten with every change
     if(_file != null) {
-      _write();
+      _file.writeAsString(_history.join('\n'));
     }
+
+    // This method always changes the history set
     return true;
   }
 }
