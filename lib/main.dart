567e793057edfd500222da13eb5c34208291956b
Steven Ortiz <sortiz832@gmail.com>
2020-03-24 22:46:15 -0500

    Updated theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:whoisit/history.dart';
 import 'package:whoisit/widgets/app.dart';
 
 Future<void> main() async {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ),
+  );
   WidgetsFlutterBinding.ensureInitialized();
   runApp(App(history: await History.fromStorage()));
 }
